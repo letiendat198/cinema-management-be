4843836ddc95e5b746ec5eef98f775d84945dd84
Le Tien Dat <le.tien.dat.198@gmail.com>
2025-03-14 22:53:33 +0700

    Project structure

---
+++ README.md
@@ -0,0 +1,20 @@
+# cinema-management-be
+
+## API endpoints
+
+/movie
+/cinema
+/cinema/room
+/seat
+	/seat/price
+/schedule
+/ticket
+/user
+/auth
+	/auth/login
+	/auth/register
+/rank
+/buy
+/item
+
+
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const movie_router = require('./routes/movie.router')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/movie', movie_router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
